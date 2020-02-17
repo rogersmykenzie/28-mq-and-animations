7700d6e55d3ce4033fa8c1d7322709757f4f91c7
Mykenzie <Work@Mykenzies-MacBook-Pro.local>
2020-02-17 12:25:12 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './App2.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuIsOpen: false
+    }
+  }
+  handleClick = () => {
+    this.setState({menuIsOpen: !this.state.menuIsOpen})
+  }
+  render() {
+    return (
+      <div>
+        <nav>
+          <h2>Start Bootstrap</h2>
+          <ul className="nav-links">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+          <img 
+          onClick={this.handleClick}
+          width="50px" 
+          src="https://i.ya-webdesign.com/images/hamburger-menu-icon-png-white-6.png" />
+        </nav>
+        <menu className={this.state.menuIsOpen === true ? "menu-open" : ""}>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </menu>
+      </div>
+    )
+  }
 }
 
 export default App;
